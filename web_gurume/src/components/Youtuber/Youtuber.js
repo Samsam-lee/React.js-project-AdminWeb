6016674225373d22ca4d3bc4244a4bfb8be72130
Seunghyeong Lee <seunghyeong27@gmail.com>
2020-11-17 02:00:25 +0900

    Feature :: GraphQL query 문 적용

--- web_gurume/src/components/Youtuber/Youtuber.js
+++ web_gurume/src/components/Youtuber/Youtuber.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import {useQuery} from "@apollo/react-hooks"
+import query from "./query"
+
+const User = () =>{
+    const { loading, error, data } = useQuery(query);
+	if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error Message : {error.message}</p>;
+    
+    const {ytbChannel} = data;
+
+	return (
+        <>
+		{ytbChannel.map(v => <>
+        <p> {v.ytbHits} {v.ytbLinkAddress} </p><p> {v.ytbProfile}  {v.ytbChannel} </p>
+        </>)}
+        </>
+	)
+}
 
 const Youtuber = () => {
     return (
-        <div>
-            
-        </div>
+        <User />
     )
 }
 
